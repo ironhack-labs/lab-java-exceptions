ef1faef4f15541ef5d942346bd970fc59d4c9dc6
celularfreak <celularfreak@hotmail.com>
2024-02-13 19:57:16 +0100

    first push

---
+++ src/main/java/Person.java
@@ -0,0 +1,69 @@
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String occupation;
+
+    public Person(int id, String name, int age, String occupation) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public boolean equals(Person otherperson){
+        return this.name.equals(otherperson.name) && this.age == otherperson.age && this.occupation.equals(otherperson.occupation);
+    }
+
+    public void printPerson(Person person){
+        System.out.println(person.toString());
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", occupation='" + occupation + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/PersonList.java
@@ -0,0 +1,35 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PersonList {
+    private Person[] personList;
+
+    public PersonList(Person[] personList) {
+        this.personList = personList;
+    }
+
+    public Person findByName( String name) throws IllegalArgumentException{
+        if (!name.matches("[a-zA-Z]+\\s[a-zA-Z]+")){
+            throw new IllegalArgumentException("Invalid name format. Use Firstname Lastname");
+        }
+        for (Person person : personList) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        throw new IllegalArgumentException("Person not found");
+    }
+
+    public Person clonePerson(Person originalperson, int newId){
+        return new Person(newId, originalperson.getName(), originalperson.getAge(), originalperson.getOccupation());
+    }
+
+    public void writePersonToFile(Person person, String filepath){
+        try (FileWriter writer = new FileWriter(filepath){
+            writer.write(person.toString());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
